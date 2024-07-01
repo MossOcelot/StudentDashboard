76a10f36ede6da658e20d9415daef3af160c341d
Student <student@coe.psu.ac.th>
2024-07-01 14:53:54 +0700

    add data and test file

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ import plotly.express as px
 import pandas as pd
 
 df = pd.read_json("C:/Users/student/Desktop/Plotly363/studentdashboard/pp3-4_2566_province.json")
+df_change_province_name = pd.read_json("C:/Users/student/Desktop/Plotly363/studentdashboard/api_province.json")
+df_thailand_location = pd.read_json("C:/Users/student/Desktop/Plotly363/studentdashboard/thailand.json")
+result_dict = dict(zip(df_change_province_name['name_th'], df_change_province_name['name_en']))
+
+df['schools_province'] = df['schools_province'].map(result_dict)
+
+
 app = Dash()
 
 app.layout = [
